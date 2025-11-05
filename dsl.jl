5c122c5b119b18f9d721e4f57fe4f3fe6a789dcf
Drew Allen McNeely <drew.mcneely@utexas.edu>
2025-11-04 22:55:32 -0500

    Added Project.toml and currently messing around with graphs

--- dsl.jl
+++ dsl.jl
@@ -1,17 +1,29 @@
-module Sandbox
-export @kerneldef
-export morphism_parsing_pipeline
-export pipenotation_to_morphisms
-export @vars
-export to_string
+module DSL
+export @kerneldef , morphism_parsing_pipeline , pipenotation_to_morphisms , @vars , to_string
 
 import Base: |
+
+import Graphs
+import GraphsMatching
 
 # This is just a wrapper for symbols.
 struct Var
 	name::Symbol
 end
 Base.show(io::IO, v::Var) = print(io, v.name)
+
+"""
+Macro that lets you define variables. 
+Usage: @vars x y z
+Turns into:
+x = Var(:x)
+y = Var(:y)
+z = Var(:z)
+"""
+macro vars(names...)
+	assigns = [:( $(esc(n)) = Var($(QuoteNode(n)))) for n in names]
+	Expr(:block, assigns...)
+end
 
 """
 A signature is a pair of lists of Vars.
@@ -42,44 +54,50 @@ struct Kernel
 	name::Symbol
 	signature::Signature
 end
+inputs(k::Kernel) = k.signature.source
+outputs(k::Kernel) = k.signature.target
 
 function copy(var::Var)
 	name = Symbol(:copy_, var.name)
 	sig = [var, var] | var
 	Kernel(name, sig)
 end
-sum(var::Var) = sum(var.name)
 
 function discard(var::Var)
 	name = Symbol(:discard_, var.name)
-	sig = [] | var
+	sig = Var[] | var
 	Kernel(name, sig)
 end
 
-"""
-Macro that lets you define variables. 
-Usage: @vars x y z
-Turns into:
-x = Var(:x)
-y = Var(:y)
-z = Var(:z)
-"""
-macro vars(names...)
-	assigns = [:( $(esc(n)) = Var($(QuoteNode(n)))) for n in names]
-	Expr(:block, assigns...)
+sum(var::Var) = discard(var)
+
+struct Port
+	var::Var
+	box::Symbol
+end
+input_ports(k::Kernel) = [Port(var, k.name) for var in inputs(k)]
+output_ports(k::Kernel) = [Port(var, k.name) for var in outputs(k)]
+
+function wires(ks::Vector{Kernel})
+	inputs = vcat(input_ports.(ks))
+	outputs = vcat(output_ports.(ks))
+	function possible_match(output::Port, input::Port)
+		(input.var == output.var) && !(input.box == output.box)
+	end
+	function possible_wires(output::Port, inputs::Vector{Port})
+		[(output, input) for input in inputs if possible_match(output, input)]
+	end
+	vcat((o -> possible_wires(o, inputs)).(outputs))
 end
 
 macro kerneldef(exp)
 	@assert exp.head == :(=) 
 	lhs, rhs = exp.args
 	@assert lhs.head == :call #Left side must look like f(y|x)
-	#fname = lhs.args[1]
-	#outer_signature = lhs.args[2]
+	fname = lhs.args[1]
+	outer_signature = lhs.args[2]
 	#return :(morphism_parsing_pipeline($(QuoteNode(fname)), $(esc(outer_signature)), $(Expr(:quote, rhs))))
-	dump(lhs)
-	dump(rhs)
-	return
-	#return :(morphism_parsing_pipeline($(Expr(:quote, lhs)), $Expr(:quote, rhs)))
+	return 
 end
 
 """
@@ -100,6 +118,11 @@ end
 function pipenotation_to_morphisms(expr::Expr)
 	print(expr)
 end
+
+@vars x y z
+g = Kernel(:g, (z | y));
+f = Kernel(:f, (y | x));
+test_morphisms = [f, g, copy(y), discard(y)]
 
 
 end # module
