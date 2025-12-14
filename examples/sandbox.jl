9166aa5458e2f6221099fd526ab11e2d5e643f93
Drew Allen McNeely <drew@drewmcneely.net>
2025-12-14 15:16:32 -0500

    Added some WiringDiagram graphviz experiments to sandbox.jl

--- examples/sandbox.jl
+++ examples/sandbox.jl
@@ -2,16 +2,75 @@ using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 
 using MarkovCats
-include("../test/fixtures.jl")
+#include("../test/fixtures.jl")
 
-import .Fixtures.Compose as FC
-upg = FC.pg
-apg = FC.auto_matched_pg
-mpg = FC.manual_matched_pg
+# using Catlab.WiringDiagrams
+# using Catlab.Graphics
+# import Catlab.Graphics.ComposeWiringDiagrams as CompWD
+# import Catlab.Graphics.GraphvizWiringDiagrams as GWD
+# import GraphViz as GV
+# 
+# using Cairo, Fontconfig
+# using Compose
+# using GraphPlot
+# using Graphs
+# using Colors  # only if you want colors; you can remove this
 
-println(portgraph_signature(apg))
-println(portgraph_signature(mpg))
-println(equivalent_portgraphs(apg, mpg))
+using Catlab.WiringDiagrams
+
+using Catlab.Graphics
+import Catlab.Graphics: Graphviz, GraphvizGraphs
+
+
+show_diagram(d::WiringDiagram) = to_graphviz(d,
+  orientation=LeftToRight,
+  labels=true, label_attr=:xlabel,
+  node_attrs=Graphviz.Attributes(
+    :fontname => "Courier",
+  ),
+  edge_attrs=Graphviz.Attributes(
+    :fontname => "Courier",
+  )
+)
+
+# function plot_diagram(d::WiringDiagram)
+# 	pic = CompWD.to_composejl(d)
+# 	draw(PNG("diagram.png", pic.width, pic.height), pic.context)
+# end
+
+f = Box(:f, [:A], [:B])
+g = Box(:g, [:B], [:C])
+h = Box(:h, [:C], [:D])
+
+d = WiringDiagram([:A], [:C])
+
+fv = add_box!(d, f)
+gv = add_box!(d, g)
+
+add_wires!(d, [
+  (input_id(d),1) => (fv,1),
+  (fv,1) => (gv,1),
+  (gv,1) => (output_id(d),1),
+])
+
+g = show_diagram(d)
+# Graphviz.pprint(stdout, g)
+
+filepath = "graph.png"
+# gv = GraphvizGraphs.to_graphviz(d)
+gv = show_diagram(d)
+open(filepath, "w") do io
+	Graphviz.run_graphviz(io, gv, format="png")
+end
+
+# import .Fixtures.Compose as FC
+# upg = FC.pg
+# apg = FC.auto_matched_pg
+# mpg = FC.manual_matched_pg
+# 
+# println(portgraph_signature(apg))
+# println(portgraph_signature(mpg))
+# println(equivalent_portgraphs(apg, mpg))
 
 # @kernelassignments begin
 # 	# Kernel Composition
